6cd4ceb008a8865ad87aa4c0073b5935b6ab4ea2
Larry Chisvin <larry.chisvin@oneconvergence.com>
2023-01-16 09:31:48 -0700

    Update

--- insurance/preprocess.py
+++ insurance/preprocess.py
@@ -1,3 +1,4 @@
+# %%
 import os
 import pandas as pd
 
@@ -8,35 +9,36 @@ warnings.filterwarnings("ignore")
 import requests, argparse
 requests.packages.urllib3.disable_warnings()
 
+# %% [markdown]
+# ### Set up the variables for Dataset Access
+
+# %%
 MLFLOW_EXPERIMENT_NAME = os.getenv('DKUBE_PROJECT_NAME')
 
-## Get the username & token and create the DKube SDK API ariable
+## Get the username & token and create the DKube SDK API variables
 
-SERVING_DKUBE_URL = os.getenv("DKUBE_URL")
-SERVING_DKUBE_TOKEN = os.getenv("DKUBE_USER_ACCESS_TOKEN")
-SERVING_DKUBE_USERNAME = os.getenv("DKUBE_USER_LOGIN_NAME")
+SERVING_DKUBE_URL = os.environ.get("DKUBE_URL")
+SERVING_DKUBE_TOKEN = os.environ.get("DKUBE_USER_ACCESS_TOKEN")
+SERVING_DKUBE_USERNAME = os.environ.get("DKUBE_USER_LOGIN_NAME")
+SERVING_DKUBE_PROJECT_ID = os.environ.get("DKUBE_PROJECT_ID")
+
+SERVING_DKUBE_INPUT_DATASET_NAME = "insurance-input-dataset"
+SERVING_DKUBE_OUTPUT_DATASET_NAME = "insurance-output-dataset"
 
 dapi = DkubeApi(URL=SERVING_DKUBE_URL,token=SERVING_DKUBE_TOKEN)
-pre_api = DkubePreprocessing(SERVING_DKUBE_USERNAME)
-dataset_info = DkubeDataset("larryc1200", name="ins-lc-dataset")
+dataset_input_info = DkubeDataset(SERVING_DKUBE_USERNAME, name=SERVING_DKUBE_INPUT_DATASET_NAME)
+dataset_output_info = DkubeDataset(SERVING_DKUBE_USERNAME, name=SERVING_DKUBE_OUTPUT_DATASET_NAME)
 
-#DATASET_URL could be specified as Environment parameters at the time of creating JL or Run
+# %% [markdown]
+# ### Preprocess Data
 
-# Define data
-INPUT_DATA_URL = os.getenv("DATASET_URL", "https://dkube-examples-data.s3.us-west-2.amazonaws.com/monitoring-insurance/training-data/insurance.csv")
+# %%
+## Get the original data from the input dataset repo mount point & write to output dataset
+data = pd.read_csv('/input/dataset/insurance.csv')
+# dapi.create_dataset(dataset_output_info)
 
-## Get the original data
-data = pd.read_csv(INPUT_DATA_URL)
+## Remove the AGE column & write out the new output dataset
+insurance_filtered = data.drop('age', axis=1)
+insurance_filtered.to_csv('/output/dataset/insurance.csv')
 
-## Choose which columns to keep
-insurance_filtered = data.drop(['age'], axis=1)
 
-## Set up the API parameters for creating an updated dataset
-DATASET_REPO_NAME = "ins-lc-dataset"
-DATASET_MOUNT_PATH = "/dataset/"
-
-## Create the new dataset with the filtered data
-# dapi.create_dataset(dataset_info)
-
-# pre_api.add_output_dataset(name=DATASET_REPO_NAME, mountpath=DATASET_MOUNT_PATH)
-insurance_filtered.to_csv(DATASET_MOUNT_PATH + "test.csv")
